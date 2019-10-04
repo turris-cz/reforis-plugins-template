d10009cd32ab255be38ee9daa8f06b8464cbc6af
Bogdan Bodnar <bogdan.bodnar@nic.cz>
2019-10-04 16:27:01 +0200

    Change repo name.

--- reforis-{{cookiecutter.plugin_name_minuses}}/setup.py
+++ reforis-{{cookiecutter.plugin_name_minuses}}/setup.py
@@ -5,32 +5,61 @@
 
 # !/usr/bin/env python3
 
+import copy
+import pathlib
+
 import setuptools
+from setuptools.command.build_py import build_py
+
+NAME = 'reforis_{{cookiecutter.plugin_name_snake}}'
+
+BASE_DIR = pathlib.Path(__file__).absolute().parent
+
+
+class CustomBuild(build_py):
+    def run(self):
+        # build package
+        build_py.run(self)
+
+        from reforis_distutils import ForisPluginBuild
+        cmd = ForisPluginBuild(copy.copy(self.distribution))
+        cmd.root_path = BASE_DIR
+        cmd.module_name = NAME
+        cmd.build_lib = self.build_lib
+        cmd.ensure_finalized()
+        cmd.run()
+
 
 setuptools.setup(
-    name='reforis_{{cookiecutter.plugin_name_snake}}',
+    name=NAME,
     version='{{cookiecutter.version}}',
     packages=setuptools.find_packages(exclude=['tests']),
     include_package_data=True,
 
     description='{{cookiecutter.description}}',
     author='CZ.NIC, z.s.p.o.',
+    author_email='bogdan.bodnar@nic.cz',
 
     # All versions are fixed just for case. Once in while try to check for new versions.
     install_requires=[
+        'Babel==2.7.0',
         'flask==1.0.2',
-        'Bootstrap-Flask==1.0.8',
     ],
     extras_require={
         'devel': [
-            'Babel',
             'pytest==3.7.1',
             'pylint==2.3.1',
             'pycodestyle==2.5.0',
         ],
     },
+    setup_requires=[
+        'reforis_distutils',
+    ],
+    dependency_links=[
+        "git+https://gitlab.labs.nic.cz/turris/reforis/reforis-distutils.git#egg=reforis-distutils",
+    ],
     entry_points={
-        'foris.plugins': '{{cookiecutter.plugin_name_snake}} = reforis_{{cookiecutter.plugin_name_snake}}:{{cookiecutter.plugin_name_snake}}'
+        'foris.plugins': f'{NAME} = {NAME}:diagnostics'
     },
     classifiers=[
         'Framework :: Flask',
@@ -42,5 +71,8 @@ setuptools.setup(
         'Programming Language :: Python :: 3',
         'Topic :: Internet :: WWW/HTTP :: WSGI :: Application',
     ],
+    cmdclass={
+        'build_py': CustomBuild,
+    },
     zip_safe=False,
 )
