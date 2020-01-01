9eb9561e8c806841e0986125780a8b4cae0f27af
Bogdan Bodnar <bogdan.bodnar@nic.cz>
2020-01-01 21:26:34 +0100

    Use impoved python tests.

--- reforis-{{cookiecutter.plugin_name_kebab}}/setup.py
+++ reforis-{{cookiecutter.plugin_name_kebab}}/setup.py
@@ -51,7 +51,9 @@ setuptools.setup(
             'pylint==2.3.1',
             'pylint-quotes==0.2.1',
             'pycodestyle==2.5.0',
-            'reforis @ git+https://gitlab.labs.nic.cz/turris/reforis/reforis@master#egg=reforis',
+            'reforis @ git+https://gitlab.labs.nic.cz/turris/reforis/reforis@improve-python-tests#egg=reforis',
+            'foris-client @ git+https://gitlab.labs.nic.cz/turris/foris-controller/foris-client@master#egg=foris-client',
+            'paho-mqtt==1.5.0',
         ],
     },
     setup_requires=[
