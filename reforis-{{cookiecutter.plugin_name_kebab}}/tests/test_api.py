549ea78a8dbc6da705c9af7472604f85c216c864
Aleksandr Gumroian <aleksandr.gumroian@nic.cz>
2024-03-04 14:59:50 +0100

    Format Python files with Ruff

--- reforis-{{cookiecutter.plugin_name_kebab}}/tests/test_api.py
+++ reforis-{{cookiecutter.plugin_name_kebab}}/tests/test_api.py
@@ -4,22 +4,22 @@ from http import HTTPStatus
 from reforis.test_utils import mock_backend_response
 
 
-@mock_backend_response({'example_module': {'example_action': {'key': 'value'}}})
+@mock_backend_response({"example_module": {"example_action": {"key": "value"}}})
 def test_get_example(client):
-    response = client.get('/{{cookiecutter.plugin_url_prefix}}/api/example')
+    response = client.get("/{{cookiecutter.plugin_url_prefix}}/api/example")
     assert response.status_code == HTTPStatus.OK
-    assert response.json['key'] == 'value'
+    assert response.json["key"] == "value"
 
 
-@mock_backend_response({'example_module': {'example_action': {'result': True}}})
+@mock_backend_response({"example_module": {"example_action": {"result": True}}})
 def test_post_example_invalid_json(client):
-    response = client.post('/{{cookiecutter.plugin_url_prefix}}/api/example', json=False)
+    response = client.post("/{{cookiecutter.plugin_url_prefix}}/api/example", json=False)
     assert response.status_code == HTTPStatus.BAD_REQUEST
-    assert response.json == 'Invalid JSON'
+    assert response.json == "Invalid JSON"
 
 
-@mock_backend_response({'example_module': {'example_action': {'key': 'value'}}})
+@mock_backend_response({"example_module": {"example_action": {"key": "value"}}})
 def test_post_example_backend_error(client):
-    response = client.post('/{{cookiecutter.plugin_url_prefix}}/api/example', json={'modules': []})
+    response = client.post("/{{cookiecutter.plugin_url_prefix}}/api/example", json={"modules": []})
     assert response.status_code == HTTPStatus.INTERNAL_SERVER_ERROR
-    assert response.json == 'Cannot create entity'
+    assert response.json == "Cannot create entity"
